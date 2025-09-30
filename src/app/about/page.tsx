7967a7eac6c0a2bca544135dbdb94ba450e24732
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-09-30 18:43:57 +0200

    metadata and routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+    title: "About us page",
+    description: "about us page description"
+}
 
 export default function About() {
     return <h4>About page</h4>
--- src/app/phones/[phoneId]/page.tsx
+++ src/app/phones/[phoneId]/page.tsx
@@ -1,4 +1,20 @@
-export default async function PhoneId({ params }: { params: Promise<{ phoneId: string }> }) {
+import { Metadata } from "next";
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const id = (await params).phoneId;
+    const title = await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`iphone ${id}`);
+        }, 100);
+    });
+    return {
+        title: `Phone ${id} with title ${title}`
+    }
+}
+
+type Props = { params: Promise<{ phoneId: string }> }
+
+export default async function PhoneId({ params }: Props) {
 
     const phoneId = (await params).phoneId;
     return (
