edfff103effa65f351174a344ace66d1b44fcf4b
andi-hbtn <andi.bevapi@holbertonschool.com>
2025-10-08 17:32:09 +0200

    tutorial 39

---
+++ src/app/comments/[id]/route.ts
@@ -0,0 +1,30 @@
+import { comments } from "../../comments/data";
+
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
+
+    const { id } = await params;
+
+    const comment = comments.find((coment, index) => coment.id === parseInt(id));
+
+    return new Response(
+        JSON.stringify(comment),
+        {
+            headers: { "Content-Type": "application/json" },
+            status: 201
+        }
+    );
+}
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }) {
+
+    const { id } = await params;
+    const body = await request.json();
+
+    const index = comments.findIndex((index) => index.id === parseInt(id));
+    comments[index].text = body.text
+
+    return new Response(
+        JSON.stringify(comments[index])
+    )
+
+}
